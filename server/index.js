60291ac1867ce77b7db1fb931a6bbbb748bc21b2
Mahika <mahikakushwaha@gmail.com>
2023-07-28 01:27:40 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,27 +1,34 @@
-// app.js
 const express = require('express');
-const mongodb = require('mongodb');
+const mongoose = require('mongoose');
 const app = express();
-const MongoClient = mongodb.MongoClient;
-const mongoUrl = 'mongodb://localhost:27017';
-const dbName = 'jobDB';
 const port = 3000;
 
-MongoClient.connect(
-	mongoUrl,
-	{ useNewUrlParser: true, useUnifiedTopology: true },
-	(err, client) => {
-		if (err) {
-			console.error('Error connecting to MongoDB:', err);
-			return;
-		}
+const mongoUrl = 'mongodb://localhost:27017/jobDB';
+mongoose
+	.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log('Connected to MongoDB successfully'))
+	.catch((err) => console.error('Error connecting to MongoDB:', err));
 
-		console.log('Connected to MongoDB successfully');
-		const db = client.db(dbName);
+const jobSchema = new mongoose.Schema({
+	jobTitle: String,
+	jobId: String,
+	location: String,
+	salary: String,
+	description: String,
+	applicationDate: Date,
+});
 
-		// Start the server after connecting to the database
-		app.listen(port, () => {
-			console.log(`Server is running on http://localhost:${port}`);
-		});
+const Job = mongoose.model('Job', jobSchema);
+
+app.get('/jobs', async (req, res) => {
+	try {
+		const jobs = await Job.find({});
+		res.json(jobs);
+	} catch (error) {
+		res.status(500).json({ message: 'Error fetching jobs' });
 	}
-);
+});
+
+app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
+});
