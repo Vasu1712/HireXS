47fd248884ba4600cf480006101a4379e6ec2aef
Abhishek Rao <abhishek.komarraju@gmail.com>
2023-07-28 23:01:23 +0530

    home updated

--- client/src/routes/HomeComponent.js
+++ client/src/routes/HomeComponent.js
@@ -1,13 +1,32 @@
+import logo from '../assets/images/hirexs_logo.png';
+import { Link } from "react-router-dom";
+
 const HomeComponent = () => {
     return (
-        <div className="w-full h-full bg-app-black flex">
-            <div className="w-1/5 bg-gray-800">
-                hello
+        <div className="w-full h-full bg-app-black">
+            <div className="flex p-8 justify-between font-semibold">
+                <div className='ml-10'>
+                    <Link to="/home">
+                        <img src={logo} style={{ width: "209px", height: "51px" }} />
+                    </Link>
+                </div>
+                <div className='text-white text-xl mt-3'>
+                    <Link to={"/home"} className='pr-10'>Home</Link>
+                    <Link to={"/jobs"} className='pr-10'>All Jobs</Link>
+                    <Link to={"/past"} className='pr-10'>Past</Link>
+                    <Link to={"/about"}>About</Link>
+                </div>
+                <div>
+                    <Link to="/login" className="bg-white h-2/3 px-6 mt-2 flex items-center justify-center rounded-full font-semibold cursor-pointer mr-14">
+                        Log in
+                    </Link>
+                </div>
             </div>
-            <div>
-                hello
+            <div className='text-center'>
+                <div className='text-white text-5xl'>
+                    Welcome to
+                </div>
             </div>
-            <img src="../assets/images/bg.png" />
         </div>
     )
 };
