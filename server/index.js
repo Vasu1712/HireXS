df5c1214b89ddfd81a96abd1c88379b5ad396130
Abhishek Rao <abhishek.komarraju@gmail.com>
2023-07-29 09:37:12 +0530

    backend port change

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const jobRoutes = require('./routes/jobslist');
 require('dotenv').config();
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(cors());
 app.use(express.json());
