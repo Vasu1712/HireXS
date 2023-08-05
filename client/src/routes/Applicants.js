f8518222b6a6584b5b33ef764b6596a8dedf55fc
Vasu Pal <vasupal.17.12.2002@gmail.com>
2023-08-06 01:06:11 +0530

    applicants

--- client/src/routes/Applicants.js
+++ client/src/routes/Applicants.js
@@ -2,7 +2,7 @@ import LoggedInContainer from "../containers/LoggedInContainer";
 import { Jobcard } from "./Jobcard";
 import { Icon } from "@iconify/react";
 import { Link } from "react-router-dom";
-import { Card, Typography, CardBody, CardFooter, Button } from "@material-tailwind/react";
+import { Card, Typography, Checkbox } from "@material-tailwind/react";
 
 const Applicants = () => {
     return (
@@ -11,8 +11,17 @@ const Applicants = () => {
                 <div className="mx-8 text-4xl font-semibold text-color6">
                     Applicants
                 </div>
-               <div>
-                
+                <div className="m-8 italic text-lg text-color6">
+                    Select the Applicants you wish to send assesment links to
+                </div>
+               <div className="m-8 p-2 w-2/3 flex justify-between rounded-xl bg-color1">
+               <Checkbox color="#EE4774" ripple={true} />
+               <div className="mt-3 mx-12 text-white grow">
+                Sandeep Chaudhary
+               </div>
+               <div className="mt-3 mx-12 text-white flex-none">
+                8.5
+               </div>
                </div>
             </div>
         </LoggedInContainer>
