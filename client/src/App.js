d0f9d56f70c7ea698378ad0bad0de2c27275fe6d
Abhishek <abhishek.rao@matrice.ai>
2025-01-14 20:54:02 +0530

    fixed login

--- client/src/App.js
+++ client/src/App.js
@@ -1,81 +1,53 @@
-import './output.css';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import HomeComponent from './routes/HomeComponent';
-import LoginComponent from './routes/login';
-import SignupComponent from './routes/Signup';
-import LoggedInHome from './routes/LoggedInHome.js';
-import Jobs from './routes/Jobs';
-import JobPage from './routes/JobPage';
-import Contact from './routes/Contact';
-import HRjobs from './routes/HRJobs';
-import Applicants from './routes/Applicants';
-import About from './routes/About';
-import { useCookies } from 'react-cookie';
+import "./output.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import HomeComponent from "./routes/HomeComponent";
+import LoginComponent from "./routes/login";
+import SignupComponent from "./routes/Signup";
+import LoggedInHome from "./routes/LoggedInHome.js";
+import Jobs from "./routes/Jobs";
+import JobPage from "./routes/JobPage";
+import Contact from "./routes/Contact";
+import HRjobs from "./routes/HRJobs";
+import Applicants from "./routes/Applicants";
+import About from "./routes/About";
+import { useCookies } from "react-cookie";
+import { useEffect } from "react";
 
 function App() {
-	const [cookie] = useCookies(['token']);
+  const [cookie] = useCookies(["token"]);
 
-	return (
-		<div className='w-screen h-screen font-poppins '>
-			<BrowserRouter>
-				{cookie.token && cookie.token !== undefined ? (
-					<Routes>
-						<Route
-							path='/jobs'
-							element={cookie.access === 'admin' ? (<HRjobs />) : <Jobs />}
-						/>
-						<Route
-							path='/jobs/:jobid'
-							element=<JobPage />
-						/>
-						<Route
-							path='/home'
-							element=<LoggedInHome />
-						/>
-						<Route
-							path='/jobs/applicants/:jobId'
-							element=<Applicants />
-						/>
-						<Route
-							path='/about'
-							element=<About />
-						/>
-						<Route
-							path='/help'
-							element=<Contact />
-						/>
-						<Route
-							path='*'
-							element=<LoggedInHome />
-						/>
-					</Routes>
-				) : (
-					<Routes>
-						<Route
-							path='/jobs'
-							element=<Jobs />
-						/>
-						<Route
-							path='/home'
-							element=<HomeComponent />
-						/>
-						<Route
-							path='/login'
-							element=<LoginComponent />
-						/>
-						<Route
-							path='/signup'
-							element=<SignupComponent />
-						/>
-						<Route
-							path='*'
-							element=<Navigate to='/home' />
-						/>
-					</Routes>
-				)}
-			</BrowserRouter>
-		</div>
-	);
+  useEffect(() => {
+    // This will run whenever the cookie changes
+  }, [cookie]);
+
+  return (
+    <div className="w-screen h-screen font-poppins">
+      <BrowserRouter>
+        {cookie.token && cookie.token !== undefined ? (
+          <Routes>
+            <Route
+              path="/jobs"
+              element={cookie.access === "admin" ? <HRjobs /> : <Jobs />}
+            />
+            <Route path="/jobs/:jobid" element={<JobPage />} />
+            <Route path="/home" element={<LoggedInHome />} />
+            <Route path="/jobs/applicants/:jobId" element={<Applicants />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/help" element={<Contact />} />
+            <Route path="*" element={<LoggedInHome />} />
+          </Routes>
+        ) : (
+          <Routes>
+            <Route path="/jobs" element={<Jobs />} />
+            <Route path="/home" element={<HomeComponent />} />
+            <Route path="/login" element={<LoginComponent />} />
+            <Route path="/signup" element={<SignupComponent />} />
+            <Route path="*" element={<Navigate to="/home" />} />
+          </Routes>
+        )}
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
